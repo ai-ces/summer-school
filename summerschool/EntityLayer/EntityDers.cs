6dbb32f9224bd8eef25d6a30e2ec1fcd1b2d3999
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-07-01 01:37:52 +0300

    entity layers

---
+++ summerschool/EntityLayer/EntityDers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    internal class EntityDers
+    {
+
+        private string dersad;
+
+        public string  DERSAD
+        {
+            get { return dersad; }
+            set { dersad = value; }
+        }
+
+        private int min;
+
+        public int MIN
+        {
+            get { return min; }
+            set { min = value; }
+        }
+
+        private int max;
+
+        public int MAX
+        {
+            get { return max; }
+            set { max = value; }
+        }
+
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+}
---
+++ summerschool/EntityLayer/EntityOgretmen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    internal class EntityOgretmen
+    {
+        private int ogrid;
+
+        public int OGRTID
+        {
+            get { return ogrid; }
+            set { ogrid = value; }
+        }
+
+        private int ogrtbrans;
+
+        public int OGRTBRANS
+        {
+            get { return ogrtbrans; }
+            set { ogrtbrans = value; }
+        }
+
+        private string ogrtad;
+
+        public string OGRTAD
+        {
+            get { return ogrtad; }
+            set { ogrtad = value; }
+        }
+
+    }
+}
